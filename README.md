312dcac11c57f44ccb8dba930fb1555679244002
hika019 <hkvm584@gmail.com>
2024-05-18 21:07:03 +0900

    consume Pulsar

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 
 ```bash
+sudo docker compose up db -d
+sudo docker compose up pulsar -d
 sudo docker exec pulsar bash /init.sh
+sudo docker compose up vector -d
+sudo docker exec vector bash /data/sample/sample-data.sh
 ```
 
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"time"
 
@@ -19,11 +21,45 @@ func main() {
 		log.Fatal("Could not init Config:", err.Error())
 	}
 
-	//consume(conf.Input)
-	_, err = NewDbClient(conf.Output.Rdb)
+	client, err := newPulsarClient(conf.Input)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatalln("Could not instantiate Pulsar client:", err.Error())
 	}
+
+	defer client.Close()
+
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            conf.Input.Topic,
+		SubscriptionName: "test",
+		Type:             pulsar.Shared,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer consumer.Close()
+
+	for i := 0; i < 10; i++ {
+		// may block here
+		msg, err := consumer.Receive(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Received message msgId: %#v -- content: '%s'\n",
+			msg.ID(), string(msg.Payload()))
+
+		consumer.Ack(msg)
+	}
+
+	if err := consumer.Unsubscribe(); err != nil {
+		log.Fatal(err)
+	}
+	// _, err = NewDbClient(conf.Output.Rdb)
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 }
 
@@ -40,17 +76,16 @@ func initConf() (config.Config, error) {
 	return conf, nil
 }
 
-func consume(c config.Pulsar) {
+func newPulsarClient(c config.Pulsar) (pulsar.Client, error) {
 	client, err := pulsar.NewClient(pulsar.ClientOptions{
 		URL:               c.Host,
 		OperationTimeout:  timeoutSec * time.Second,
 		ConnectionTimeout: timeoutSec * time.Second,
 	})
 	if err != nil {
-		log.Fatalln("Could not instantiate Pulsar client:", err.Error())
+		return nil, err
 	}
-
-	defer client.Close()
+	return client, nil
 }
 
 func NewDbClient(c config.Rdb) (*sql.DB, error) {
